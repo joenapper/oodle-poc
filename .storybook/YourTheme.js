c58ff662468a9832533a5526f9e57b05dc90f85b
joenapper <joe.napper@hotmail.co.uk>
2021-09-19 19:40:03 +0100

    Created basic custom Storybook theme

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+import brandImage from "../public/images/oodle.png"
+
+export default create({
+  base: 'light',
+  brandTitle: 'Oodle POC UI Library',
+  brandUrl: 'https://www.oodlecarfinance.com/',
+  brandImage,
+});
